b6251be0c2fb3b0bb670182b44be125dabf07855
LambertCOL <lambertcol@foxmail.com>
2020-02-18 12:30:32 +0800

    add cookie

--- src/main/java/life/lambert/community/community/mapper/UserMapper.java
+++ src/main/java/life/lambert/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.lambert.community.community.mapper;
 import life.lambert.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,4 +12,7 @@ import org.springframework.stereotype.Component;
 public interface UserMapper {
     @Insert("INSERT INTO USER(ACCOUNT_ID, NAME, TOKEN, GMT_CREATE, GMT_MODIFIED) VALUES(#{accountId}, #{name}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("SELECT * FROM USER WHERE token = #{token}")
+    User findByToken(@Param("token") String token);
 }
